83d7f1da529c48a96381fa5e3d93a8b2e8e6b88a
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-03-15 16:25:59 +0300

    deploy

--- cafeface/settings.py
+++ cafeface/settings.py
@@ -1,8 +1,21 @@
 import os
+from urllib.parse import quote
 
 class Config(object):
     DEBUG = False
     MONGO_URI = "mongodb://localhost:27017/cafe"
+    MONGODB_SETTINGS = {
+        "db": "cafe",
+        "host": MONGO_URI,
+    }
+
+class DevConfig(Config):
+    DEBUG = True
+
+class ProdConfig(Config):
+    MONGO_URI = os.environ.get(
+        "MONGO_URI", "mongodb://localhost:27017/cafe"
+    ).replace("<password>", quote(os.environ.get("MONGO_PASSWORD")))
     MONGODB_SETTINGS = {
         "db": "cafe",
         "host": MONGO_URI,
