f20de1581520b2f5a4b4f9ffc5d6f5972c1191eb
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-04-02 15:35:46 +0300

    fix: fixed database config

--- cafeface/settings.py
+++ cafeface/settings.py
@@ -1,9 +1,11 @@
 import os
+import urllib.parse
 
 class Config(object):
     DEBUG = False
-    MONGO_URI = "mongodb://localhost:27017/cafe"
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/cafe")
+    MONGO_PASSWORD = urllib.parse.quote_plus(os.environ.get("MONGO_PASSWORD", "password"))
     MONGODB_SETTINGS = {
         "db": "cafe",
-        "host": MONGO_URI,
+        "host": (lambda uri, password : uri.replace("<password>", password))(MONGO_URI, MONGO_PASSWORD),
     }
